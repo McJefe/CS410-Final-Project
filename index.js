06781f856972b3af4b56b3930c2bb8e8443a7fc0
Jeff McHale <jeffmchale94@gmail.com>
2022-03-14 15:38:22 -0700

    Resized opening image and fixed card colors

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById('submit').addEventListener('click', () => {
+const name = document.getElementById('inputName');
+const email = document.getElementById('inputEmail');
+const message = document.getElementById('inputMessage');
+
+alert("Name received: " + name.value + "\nEmail Received: "+ email.value+ "\nYour Message: " + message.value);
+
+// Open mail application, prefills email and message
+window.open('mailto:' + email.value +'?body=' + message.value + '&subject=Get in Touch');
+
+name.value='';
+email.value='';
+message.value='';
+
+});
+
